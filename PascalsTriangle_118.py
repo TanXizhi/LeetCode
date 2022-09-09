d5b876118d6df024f6193a1a86cd5ff708b8ca87
TanXizhi <109449112+TanXizhi@users.noreply.github.com>
2022-09-09 14:03:15 +0800

    Leetcode #118

---
+++ PascalsTriangle_118.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        PT = []
+        for i in range(numRows):
+            row = []
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(PT[i-1][j-1] + PT[i-1][j])
+            PT.append(row)
+        return PT
+
